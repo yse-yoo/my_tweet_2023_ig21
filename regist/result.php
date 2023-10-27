d49827258332330b1bfb8e39836fc977579c629c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-27 13:37:07 +0900

    update

--- regist/result.php
+++ regist/result.php
@@ -12,10 +12,15 @@ session_regenerate_id(true);
 <body>
     <?php include('../components/nav.php'); ?>
 
-    <h2>会員登録完了</h2>
-    <p>
-        登録ありがとうございました。
-    </p>
+    <div class="container">
+        <h2>会員登録完了</h2>
+        <p>
+            登録ありがとうございました。
+        </p>
+        <div class="mt-3">
+            <a class="w-100 btn btn-outline-primary" href="login/">ログイン</a>
+        </div>
+    </div>
 </body>
 
 </html>
