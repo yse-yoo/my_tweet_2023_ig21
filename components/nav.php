769859dfbba02a523b267fc672534eccd1db06c0
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-27 13:52:04 +0900

    update

--- components/nav.php
+++ components/nav.php
@@ -7,19 +7,12 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../regist/input.php">会員登録</a>
+          <a class="nav-link" href="../regist/">会員登録</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="../login/">ログイン</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
